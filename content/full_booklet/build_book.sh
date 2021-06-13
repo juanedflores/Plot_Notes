1012419fcac9d392a419e24b954a6ab9d752a9ca
Juan Eduardo Flores <juaneduardoflores@gmail.com>
2021-06-13 03:27:54 -0500

    added noise pd patch

--- content/full_booklet/build_book.sh
+++ content/full_booklet/build_book.sh
@@ -1,7 +1,11 @@
+mkdir pdfs
 cd ..
-for d in page*/; 
-	do (cd "$d" && /Users/juaneduardoflores/.pyenv/shims/vpype read "${d%?}".svg layout 14cmx19cm write book_"${d%?}".svg && /Applications/Inkscape.app/Contents/MacOS/inkscape "./book_${d%?}.svg" --export-filename=../full_booklet/pdfs/${d%?}.pdf);
+for d in page*/;
+	do (cd "$d" && /Users/juaneduardoflores/.pyenv/shims/vpype read "${d%?}".svg layout 14cmx19cm write book_"${d%?}".svg && /Applications/Inkscape.app/Contents/MacOS/inkscape "./book_${d%?}.svg" --export-filename=../full_booklet/pdfs/${d%?}.pdf && rm book_${d%?}.svg);
 done
 
 cd ./full_booklet/pdfs/
 /System/Library/Automator/Combine\ PDF\ Pages.action/Contents/Resources/join.py -o ../plotnotes.pdf $(ls -rt)
+
+cd ..
+rm -R pdfs
